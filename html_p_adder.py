27a99d4fa429fa1ca23d5a4de1fa4a11ef48b454
engrbugs <engrbugs@gmail.com>
2020-05-29 21:05:42 -0700

    'done'

--- html_p_adder.py
+++ html_p_adder.py
@@ -3,6 +3,7 @@
 
 # Variable
 input_txt_filename = 'input.txt'
+output_txt_filename = 'output.txt'
 add_string_before = '<p>'
 add_string_after = '</p>'
 trigger_character = '.'
@@ -24,6 +25,13 @@ def read_txt_file(filename):
     f = open(filename, "r")
     if f.mode == 'r':
         return f.read()
+    f.close()
+
+
+def write_txt_file(body):
+    f = open(output_txt_filename, "w+")
+    f.write(body)
+    f.close()
 
 
 def main():
@@ -38,11 +46,12 @@ def main():
                 if not firstline:
                     result += add_string_before + '\n'
                     result += add_string_after + '\n'
-                result += '\t' + line + '\n'
-                firstline = None
+        result += '\t' + line + '\n'
+        firstline = None
 
     result += add_string_after
     print(result)
+    write_txt_file(result)
 
 
 if __name__ == "__main__":
