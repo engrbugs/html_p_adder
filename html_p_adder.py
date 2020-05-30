20196739a721e7c6715078b2b199df520d688928
engrbugs <engrbugs@gmail.com>
2020-05-29 20:54:22 -0700

    '2020-05-29-2054'

--- html_p_adder.py
+++ html_p_adder.py
@@ -1,12 +1,48 @@
 #! /usr/bin/env python
 
 
-def main():
-    print('hello world')
-    f = open("input.txt", "r")
+# Variable
+input_txt_filename = 'input.txt'
+add_string_before = '<p>'
+add_string_after = '</p>'
+trigger_character = '.'
+# the Left of the triggered character must be numeric
+
+def left(s, amount):
+    return s[:amount]
+
+
+def right(s, amount):
+    return s[-amount:]
+
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+
+def read_txt_file(filename):
+    f = open(filename, "r")
     if f.mode == 'r':
-        contents = f.read()
-    print(contents)
+        return f.read()
+
+
+def main():
+    print('html adder by engrbugs')
+    contents = read_txt_file(input_txt_filename)
+    result = add_string_before + '\n'
+    firstline = True  # <--- Don't add string in the first line.
+    for line in contents.splitlines():
+        x = line.find(trigger_character)
+        if x is not -1:
+            if left(line, x).strip().isnumeric():
+                if not firstline:
+                    result += add_string_before + '\n'
+                    result += add_string_after + '\n'
+                result += '\t' + line + '\n'
+                firstline = None
+
+    result += add_string_after
+    print(result)
 
 
 if __name__ == "__main__":
